df4398decc5bc744d268b443b727a768b9406419
Lucas Santos <lucas201273@gmail.com>
2024-01-23 11:26:42 -0300

    refactor: move assets folder to a shared

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import GlobalStyles from '@app/assets/global/global'
+import GlobalStyles from '@app/shared/assets/global/global'
 import { Main } from '@app/pages'
 
 export const App = (): JSX.Element => {
--- src/pages/Main/components/Header/index.tsx
+++ src/pages/Main/components/Header/index.tsx
@@ -7,6 +7,7 @@ export function Header() {
       <nav>
         <ul>
           <li> Clientes</li>
+          <li> Pagamentos</li>
           <li> Títulos</li>
           <Dropdown text="Relatórios">
             <DropdownMenu>
