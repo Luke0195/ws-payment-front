b18954bb2c7e15e57f06096ba96018491c3e6804
Lucas Santos <lucas201273@gmail.com>
2024-01-24 11:45:36 -0300

    refactor: remove error exception to a custom class

--- src/features/payment/services/index.ts
+++ src/features/payment/services/index.ts
@@ -1,15 +1,17 @@
 import { axios } from '@app/libs/axios'
 import { PaymentForm } from '../index'
 import { PaymentModel } from '@app/entities/payment-model'
+import { InvalidRequest } from '@app/shared/errors'
 
 export const createPaymentService = async (
   data: PaymentForm,
 ): Promise<PaymentModel> => {
   try {
     const response: PaymentModel = await axios.post('/payment', data)
-    if (typeof response !== 'object') throw new Error('Invalid response')
+    if (typeof response !== 'object')
+      throw new InvalidRequest('Invalid response')
     return response
   } catch (error: any) {
-    throw new Error('Ocorreu um erro ao realizar há ação!')
+    throw new InvalidRequest('Ocorreu um erro ao realizar há ação!')
   }
 }
