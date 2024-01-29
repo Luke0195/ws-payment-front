d12fa1f1736a97c0ba2abbb1ce0752299e687585
Lucas Santos <lucas201273@gmail.com>
2024-01-29 17:24:43 -0300

    refactor: add formmatter on mappers

--- src/features/clients/mapper/index.tsx
+++ src/features/clients/mapper/index.tsx
@@ -1,21 +1,23 @@
 import { BodyInterface } from '../interfaces'
 import { Client } from '@app/entities/client-model'
 import { Icon } from '@app/libs/semantic-ui'
+import formatters from '@app/utils/formatters'
 
+const { notEmptyStringOrDefault } = formatters
 export const parsedDataToDomain = (
   data: Client[],
   showModal: (data: { id: string; name: string }) => void,
 ): BodyInterface[] => {
   return data.map((item) => {
     const parsed: BodyInterface = {
-      id: item.id,
-      name: item.name,
-      address: item.address,
-      city: item.city,
-      code: item.code,
-      email: item.email,
-      phone: item.phone,
-      sublocallity: item.sub_locality,
+      id: notEmptyStringOrDefault(item.id),
+      name: notEmptyStringOrDefault(item.name),
+      address: notEmptyStringOrDefault(item.address),
+      city: notEmptyStringOrDefault(item.city),
+      code: notEmptyStringOrDefault(item.code),
+      email: notEmptyStringOrDefault(item.email),
+      phone: notEmptyStringOrDefault(item.phone),
+      sublocallity: notEmptyStringOrDefault(item.sub_locality),
       actions: null,
     }
 
